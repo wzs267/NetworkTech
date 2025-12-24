a7317e65f439bba89254c6874b476d1a239f3e19
Zeshun <10266354@gitlab.com>
2025-12-25 00:04:26 +0800

    lab5

---
+++ LAB5/router.h
@@ -0,0 +1,176 @@
+﻿#pragma once
+#include <Winsock2.h>
+
+#include "pcap.h"
+#include "stdio.h"
+
+#include <string.h>
+
+
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib, "packet.lib")
+#pragma comment(lib,"wpcap.lib")
+#pragma comment(lib,"wsock32.lib")
+#pragma warning(disable : 4996)
+
+#pragma pack(1)
+
+typedef struct FrameHeader_t {
+	BYTE DesMAC[6];
+	BYTE SrcMAC[6];
+	WORD FrameType;
+}FrameHeader_t;
+
+typedef struct IPHeader_t {
+	BYTE Ver_HLen;
+	BYTE TOS;
+	WORD TotalLen;
+	WORD ID;
+	WORD Flag_Segment;
+	BYTE TTL;
+	BYTE Protocol;
+	WORD Checksum;
+	u_int SrcIP;
+	u_int DstIP;
+}IPHeader_t;
+
+typedef struct ARPFrame_t {
+	FrameHeader_t FrameHeader;
+	WORD HardwareType;
+	WORD ProtocolType;
+	BYTE HLen;
+	BYTE PLen;
+	WORD Operation;
+	BYTE SendHa[6];
+	DWORD SendIP;
+	BYTE RecvHa[6];
+	DWORD RecvIP;
+}ARPFrame_t;
+
+typedef struct Data_t {
+	FrameHeader_t FrameHeader;
+	IPHeader_t IPHeader;
+}Data_t;
+
+typedef struct ICMP {
+	FrameHeader_t FrameHeader;
+	IPHeader_t IPHeader;
+	char buf[0x80];
+}ICMP_t;
+
+#pragma pack()
+
+class arpitem
+{
+public:
+	DWORD ip;
+	BYTE mac[6];
+};
+
+class ipitem
+{
+public:
+	DWORD sip, dip;
+	BYTE smac[6], dmac[6];
+};
+
+class Routerlog
+{
+public:
+	int index;
+	char type[5];
+	ipitem ip;
+	arpitem arp;
+	Routerlog()
+	{
+		fp = fopen("log.txt", "a+");
+	}
+	~Routerlog()
+	{
+		fclose(fp);
+	}
+	static int num;
+	static Routerlog diary[50];
+	static FILE* fp;
+	static void WritelogARP(ARPFrame_t* t)
+	{
+		fprintf(fp, "ARP\t");
+		in_addr addr;
+		addr.s_addr = t->SendIP;
+		char* temp = inet_ntoa(addr);
+		fprintf(fp, "IP:\t");
+		fprintf(fp, "%s\t", temp);
+		fprintf(fp, "MAC:\t");
+		for (int i = 0; i < 6; i++)
+		{
+			fprintf(fp, "%02x:", t->SendHa[i]);
+		}
+		fprintf(fp, "\n");
+
+	}
+	static void WritelogIP(const char* a, Data_t* t)
+	{
+		fprintf(fp, "IP\t");
+		fprintf(fp, a);
+		fprintf(fp, "\t");
+		in_addr addr;
+		addr.s_addr = t->IPHeader.SrcIP;
+		char* temp = inet_ntoa(addr);
+		fprintf(fp, "ԴIP\t");
+		fprintf(fp, "%s\t", temp);
+		fprintf(fp, "ĿIP\t");
+		addr.s_addr = t->IPHeader.DstIP;
+		fprintf(fp, "%s\t", temp);
+		fprintf(fp, "ԴMAC\t");
+		for (int i = 0; i < 6; i++)
+			fprintf(fp, "%02x:", t->FrameHeader.SrcMAC[i]);
+		fprintf(fp, "ĿMAC\t");
+		for (int i = 0; i < 6; i++)
+			fprintf(fp, "%02x:", t->FrameHeader.DesMAC[i]);
+		fprintf(fp, "\n");
+
+	}
+	static void print()
+	{
+		for (int i = 0; i < num; i++)
+		{
+			printf("%d ", diary[i].index);
+			printf("%s\t ", diary[i].type);
+			if (strcmp(diary[i].type, "ARP") == 0)
+			{
+				in_addr addr;
+				addr.s_addr = diary[i].arp.ip;
+				char* temp = inet_ntoa(addr);
+				printf("%s\t", temp);
+				for (int i = 0; i < 6; i++)
+				{
+					printf("%02x.", diary[i].arp.mac[i]);
+				}
+
+
+			}
+			else if (strcmp(diary[i].type, "IP") == 0)
+			{
+				in_addr addr;
+				addr.s_addr = diary[i].ip.sip;
+				char* temp = inet_ntoa(addr);
+				printf("ԴIP%s\t", temp);
+				addr.s_addr = diary[i].ip.dip;
+				temp = inet_ntoa(addr);
+				printf("ĿIP%s\t", temp);
+				printf("ԴMAC: ");
+				for (int i = 0; i < 6; i++)
+				{
+					printf("%02x.", diary[i].ip.smac[i]);
+				}
+				printf("ĿMAC: ");
+				for (int i = 0; i < 6; i++)
+				{
+					printf("%02x.", diary[i].ip.dmac[i]);
+				}
+
+
+			}
+		}
+	}
+};
